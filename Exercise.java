6e12df55a2b8ff3df1f6f20e776c28eec3f91063
ThiangSY <44707263+ThiangSY@users.noreply.github.com>
2018-11-15 23:10:49 -0800

    Create Exercise.java

---
+++ Exercise.java
@@ -0,0 +1,9 @@
+//Create the Exercise class and compile a list of 2-4 exercises that
+//focus on each major body part (quads, calfs, biceps, lats, etc). 
+//Once this list is created, implement each exercise as its own class
+//and have them extend the Exercise class.
+
+public class Exercise {
+
+}
+
