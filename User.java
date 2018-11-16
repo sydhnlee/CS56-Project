6d7a094205bab91ed1ac1982e2884d78006ca2b5
jojeda97 <40078612+jojeda97@users.noreply.github.com>
2018-11-16 15:47:00 -0800

    Update User.java

--- User.java
+++ User.java
@@ -11,6 +11,9 @@ public class User {
     int fat;
     int protein;
     int carbs;
+    private static final int SEDENTARY = 1;
+    private static final int MODERATELY_ACTIVE = 2;
+    private static final int VERY_ACTIVE = 3;
 
 
     User(String name, int age, int weight, double height, int activity, char gender)
@@ -21,11 +24,39 @@ public class User {
         this.height = height;
         this.activity = activity;
         this.gender = gender;
+        calculateTDEE(age,weight,height,activity,gender);
     }
 
     void calculateTDEE(int age, int weight, double height, int activity, char gender)
     {
+        double w = weight/ 2.20462;
+        double cm = 2.54 * height;
+        double ree = 0;
 
+if(gender=='M')
+{ 
+ //10 x weight (kg) + 6.25 x height (cm) – 5 x age (y) + 5 = REE
+
+        ree = (10*w) + (6.25 * cm) - (5 * age) +5;
+}
+else if(gender =='F')
+{
+//10 x weight (kg) + 6.25 x height (cm) – 5 x age (y) – 161 = REE
+        ree = (10*w) +(6.25*cm)-(5*age)-161;
+}
+    if(activity == SEDENTARY)
+{
+        this.tdee = ree * 1.2;
+}
+    else if(activity == MODERATELY_ACTIVE)
+{
+     this.tdee = ree*1.55;
+}
+    else if(activity == VERY_ACTIVE)
+{
+        this.tdee = ree*1.725;
+
+}
     }
 
     void calculateMacros(double tdee)
