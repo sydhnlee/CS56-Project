df9b5aedd4b4d1a00e0ad728bbe310b14b17920a
sydhnlee <44503651+sydhnlee@users.noreply.github.com>
2018-11-15 20:59:01 -0800

    Add files via upload

--- Controller.java
+++ Controller.java
@@ -1,4 +1,20 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.application.Application;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+
+public class Controller{
+
+@FXML
+    private void switchToBody(MouseEvent event){
+        //Stage primaryStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        //primaryStage.setScene(new Scene);
+
+    }
 }
