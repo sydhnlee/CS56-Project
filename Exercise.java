0c4979e2f3b6783524a0c5b5f8fe8709d6f3acf2
ThiangSY <44707263+ThiangSY@users.noreply.github.com>
2018-11-25 16:41:16 -0800

    Update Exercise.java

--- Exercise.java
+++ Exercise.java
@@ -2,8 +2,15 @@
 //focus on each major body part (quads, calfs, biceps, lats, etc). 
 //Once this list is created, implement each exercise as its own class
 //and have them extend the Exercise class.
-
-public class Exercise {
-
+//-Soon Yang
+public class Exercise{
+  //Instance variables
+  //what kind of instance variables should there be?
+  String Bodypart
+  
+  public Exercise(String Bodypart){ 
+    //Constructor that takes a particular body part
+    
+  }
+  
 }
-
